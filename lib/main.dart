df1f4ff3b67486d2f33b313f49f7b2115f8032fd
ghiyazt <120933509+ghiyaazt@users.noreply.github.com>
2024-10-31 19:33:07 +0700

    update 311024

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-class AplikasiCuaca extends StatelessWidget {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LayarLogin(),
+      home: LoginPage(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,149 +1,64 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'home.dart';
-import 'register_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class LayarLogin extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-  Future<void> signIn(BuildContext context) async {
-    String email = emailController.text.trim();
-    String password = passwordController.text.trim();
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-      if (userCredential.user != null) {
-        // Navigasi ke halaman cuaca setelah login sukses
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => CloudHomeApp()),
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  Future<void> _signInWithEmailAndPassword() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
         );
+        // Navigate to the home screen after successful login
+        Navigator.pushReplacementNamed(context, '/home');
+      } on FirebaseAuthException catch (e) {
+        // Handle sign-in errors
+        if (e.code == 'user-not-found') {
+          // Display error message for user not found
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('No user found with this email.'),
+            ),
+          );
+        } else if (e.code == 'wrong-password') {
+          // Display error message for wrong password
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Wrong password provided.'),
+            ),
+          );
+        }
       }
-    } on FirebaseAuthException catch (e) {
-      // Tampilkan pesan error
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error: ${e.message}")),
-      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.blue[300]!, Colors.blue[900]!],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/images/logo3 2.png', // Ganti dengan nama file gambar Anda
-                    width: 100,
-                    height: 100,
-                  ),
-                  SizedBox(height: 10),
-
-                  // Kolom Email
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder(),
-                      prefixIcon: ImageIcon(
-                        AssetImage('assets/images/profile.png'),
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-
-                  // Kolom Password
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Kata Sandi",
-                      border: OutlineInputBorder(),
-                      prefixIcon: ImageIcon(
-                        AssetImage('assets/images/padlock.png'),
-                        color: Colors.black,
-                      ),
-                      suffixIcon: Icon(Icons.visibility_off),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-
-                  // Tombol Masuk
-                  ElevatedButton(
-                    onPressed: () {
-                      signIn(context); // Panggil fungsi signIn
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Masuk",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-
-                  // Tombol Kembali
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      "Kembali",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-
-                  // Tombol Register
-                  SizedBox(height: 10),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LayarRegister(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "Daftar",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+      appBar:
